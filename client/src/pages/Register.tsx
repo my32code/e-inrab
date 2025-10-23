8d2add3e3d93434dbac3b5e83ba9c88169e9986a
my32code <158629863+my32code@users.noreply.github.com>
2025-10-23 15:28:09 +0100

    Update Register.tsx

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -37,7 +37,7 @@ export function Register() {
 
   const onSubmit = async (data: RegisterFormData) => {
     try {
-      const response = await fetch('http://localhost:3000/api/auth/register', {
+      const response = await fetch('${import.meta.env.VITE_API_URL}/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
