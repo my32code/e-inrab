1d6990fe55b56d3b7e7dcd80f1e3815d732f59f4
my32code <158629863+my32code@users.noreply.github.com>
2025-10-23 11:10:49 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3001' // Proxy vers le backend
-    }
+      '/api': 'http://localhost:3001', // pour le dev local
+    },
   },
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    port: 4173,
+    host: '0.0.0.0',
+    allowedHosts: ['client-production-afb0.up.railway.app'],
+  },
 });
