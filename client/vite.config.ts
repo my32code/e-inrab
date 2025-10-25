a39821159141df6c96f116b2c33d7ab641a3958b
my32code <158629863+my32code@users.noreply.github.com>
2025-10-25 11:17:56 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,11 +16,4 @@ export default defineConfig({
     host: '0.0.0.0',
     allowedHosts: ['client-production-afb0.up.railway.app'],
   },
-  define: {
-    __API_BASE_URL__: JSON.stringify(
-      process.env.NODE_ENV === 'production'
-        ? 'https://serveur-production-59e9.up.railway.app'
-        : 'http://localhost:3001'
-    ),
-  },
 });
